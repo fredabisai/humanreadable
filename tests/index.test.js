6c875a6f75036fd8d190f14255af2d34e257694a
Fredrick Abisai <fredabisai@gmail.com>
2021-11-03 07:37:03 +0300

    Add test of throwing an error for function list

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 const { expect, test } = require('@jest/globals');
-const  {list} = require('../index');
+const  {list, hoursBySeconds} = require('../index');
 /* Tests for list function */
 test("Passing array [1, 2, 3] in list function should return text '1, 2 and 3'", () => {
     expect(list([1,2,3])).toEqual('1, 2 and 3');
@@ -13,7 +13,17 @@ test("Passing array ['Apple'] in list function should return text 'Apple'", () =
 test("Passing array ['Apple'] in list function should return text 'Apple'", () => {
     expect(list(['Apple'])).toEqual('Apple');
 });
-test("Passing a string or number type should return the same data", () => {
-    expect(list('Bike')).toEqual('Bike');
-    expect(list(10)).toEqual(10);
+test("Passing a string or number type should throw error", () => {
+    try {
+        list('Bike');
+    } catch (e) {
+        expect(e.message).toBe('arr is not array of string or number');
+    }
+    try {
+        list(10);
+    } catch (e) {
+        expect(e.message).toBe('arr is not array of string or number');
+    }
+
+
 });
