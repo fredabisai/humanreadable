40097c5ba8b286cd45a7426337e4891cc6fc1c48
Fredrick Abisai <fredabisai@gmail.com>
2021-11-03 07:46:51 +0300

    Implement hoursSeconds function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {zeroPad} = require('./utils/main.util');
 /**
  * @param {Array.<string | number>} arr - Array of items
  */
@@ -7,4 +8,40 @@ exports.list = function(arr) {
              : [arr.slice(0, arr.length -1).join(', '), arr[arr.length - 1]].join(' and ')
     }
     throw new Error('arr is not array of string or number');
+}
+/**
+ * @param {number} seconds - Time Seconds
+ * @param {('LONG' | 'MEDIUM' | 'SHORT')} format - Time Seconds
+ * @return {string} - Hours
+ */
+exports.hoursBySeconds = function(seconds, format) {
+    if(typeof seconds === 'number') {
+        try {
+            const hours = Math.floor(seconds / 3600);
+            const minutes = Math.floor(seconds % 3600 / 60);
+            const secs = Math.floor(seconds % 3600 % 60);
+            let hoursToDisplay = '';
+            let minutesToDisplay = '';
+            let secondsToDisplay = '';
+            if(format === 'LONG') {
+                hoursToDisplay =  hours > 0 ? hours + (hours === 1 ? " hour, " : " hours, ") : "0 hours, ";
+                minutesToDisplay = minutes > 0 ? minutes + (minutes === 1 ? " minute, " : " minutes, ") : "0 minutes, ";
+                secondsToDisplay = secs > 0 ? secs + (secs === 1 ? " second" : " seconds") : "0 seconds";
+            } else if(format === 'MEDIUM') {
+                hoursToDisplay =  hours > 0 ? hours + (hours === 1 ? " hr, " : " hrs, ") : "0 hrs, ";
+                minutesToDisplay = minutes > 0 ? minutes + (minutes === 1 ? " min, " : " mins, ") : "0 mins, ";
+                secondsToDisplay = secs > 0 ? secs + (secs === 1 ? " sec" : " secs") : "0 secs";
+            } else if(format === 'SHORT') {
+                hoursToDisplay = `${zeroPad(hours, 2).toString()}:`;
+                minutesToDisplay = `${zeroPad(minutes, 2).toString()}:`;
+                secondsToDisplay = zeroPad(secs, 2).toString();
+            } else {
+                throw new Error('Invalid format, Format should either be LONG, MEDIUM or SHORT');
+            }
+            return `${hoursToDisplay}${minutesToDisplay}${secondsToDisplay}`
+        } catch(error) {
+            throw error;
+        }
+    }
+    throw new Error('seconds is not a number');
 }
