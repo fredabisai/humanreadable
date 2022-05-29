62bd709ce35d8c882e2cf077756e681ca75103f0
Fredrick Abisai <fredabisai@gmail.com>
2022-05-29 16:34:44 +0300

    change zeroPad to arrow function

--- src/utils/main.util.ts
+++ src/utils/main.util.ts
@@ -1,2 +1,2 @@
-export const zeroPad = function(num: number, places: number): string {const zero = places - num.toString().length + 1;
+export const zeroPad = (num: number, places: number): string  => {const zero = places - num.toString().length + 1;
     return Array(+(zero > 0 && zero)).join("0") + num;}
