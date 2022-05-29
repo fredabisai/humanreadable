90801fe98f4253f649b432b6a474696d9419e4ab
Fredrick Abisai <fredabisai@gmail.com>
2022-05-29 17:18:36 +0300

    Reimplement throwWrongFormatError function

--- src/utils/main.util.ts
+++ src/utils/main.util.ts
@@ -1,2 +1,48 @@
-export const zeroPad = (num: number, places: number): string  => {const zero = places - num.toString().length + 1;
-    return Array(+(zero > 0 && zero)).join("0") + num;}
+export const longMonths: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+export const shortMonths: string[] = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+export const zeroPad = (num: number, places: number): string => {
+  const zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join('0') + num;
+};
+export const throwWrongFormatError = (format: 'LONG' | 'MEDIUM' | 'SHORT', hasMedium: boolean = true) => {
+  const formats: string[] = ['LONG', 'MEDIUM', 'SHORT'];
+  const hasMediumMsg: string = 'Invalid format, Format should either be LONG, MEDIUM or SHORT';
+  const hasNoMediumMsg: string = 'Invalid format, Format should either be LONG or SHORT';
+  if (formats.indexOf(format) > -1) {
+    if (!hasMedium && format === 'MEDIUM') {
+      throw new Error(hasNoMediumMsg);
+    }
+  } else {
+    if (hasMedium) {
+      throw new Error(hasMediumMsg);
+    } else {
+      throw new Error(hasNoMediumMsg);
+    }
+  }
+};
